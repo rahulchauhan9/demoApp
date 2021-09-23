7035f2b00c3114dbcdad98befe5f31885e45e3c1
Rahul Chauhan <rahul.chauhan@mtxb2b.com>
2021-09-23 17:31:46 +0530

    after push notif code break

--- src/components/InsideScreen.js
+++ src/components/InsideScreen.js
@@ -25,12 +25,12 @@ const InsideScreen = ({navigation}) => {
   };
 
   return (
-    <View style={Styles.container}>
+    <View style={styles.container}>
       <ImageBackground
         source={{
           uri: 'https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
         }}
-        style={Styles.imagebg}
+        style={styles.imagebg}
         blurRadius={50}>
         <Text style={styles.textStyle}>Inside Screen 2</Text>
 
